0e310b91faa10818568f96fbfa111aaee1959d18
BartDeCaluwe <de.caluwe.bart@gmail.com>
2017-01-03 19:56:55 +0100

    Imperial to metric switch added (experimental)

--- src/pages/coffee-ratio/coffee-ratio.ts
+++ src/pages/coffee-ratio/coffee-ratio.ts
@@ -19,36 +19,56 @@ export class CoffeeRatioPage {
   water: number = this.ratio * this.coffee;
   maxCoffee: number = 1000 / this.ratio;
   showDecimals: boolean = true;
+  metric: boolean = true;
+  gramsToOz: number = 0.03527396;
+  gramsToFlOz: number = 0.03381402;
 
-  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {}
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) { }
 
   ionViewDidLoad() {
     console.log('Hello CoffeeRatioPage Page');
   }
 
-  calculateWater(){
+  calculateWater() {
     this.calculateMaxCoffee();
-    this.water = this.ratio * this.coffee;
+    if (this.showDecimals) {
+      this.water = parseFloat((this.coffee * this.ratio).toFixed(2));
+    } else {
+      this.water = Math.round(this.coffee * this.ratio);
+    }
   }
 
-  calculateCoffee(){
+  calculateCoffee() {
     this.calculateMaxCoffee();
-    if(this.showDecimals){
+    if (this.showDecimals) {
       this.coffee = parseFloat((this.water / this.ratio).toFixed(2));
     } else {
       this.coffee = Math.round(this.water / this.ratio);
     }
   }
 
-  calculateMaxCoffee(){
-    this.maxCoffee = 1000 / this.ratio;
+  calculateMaxCoffee() {
+    if (!this.metric) {
+      this.maxCoffee = 40 / this.ratio;
+    } else {
+      this.maxCoffee = 1000 / this.ratio;
+    }
   }
 
-  openSettingsModal(){
-    let settings = this.modalCtrl.create(SettingsModalPage, { showDecimals: this.showDecimals });
+  metricToImperial() {
+    this.coffee = this.coffee * this.gramsToOz;
+    this.water = this.water * this.gramsToFlOz;
+  }
+
+  openSettingsModal() {
+    let settings = this.modalCtrl.create(SettingsModalPage, { showDecimals: this.showDecimals, metric: this.metric });
     settings.onDidDismiss(data => {
-      this.showDecimals = data;
+      this.showDecimals = data.showDecimals;
+      this.metric = data.metric;
       this.calculateCoffee();
+      if (!this.metric) {
+        this.metricToImperial();
+      }
     });
     settings.present();
   }
--- src/pages/settingsModal/settingsModal.ts
+++ src/pages/settingsModal/settingsModal.ts
@@ -13,12 +13,14 @@ import { NavController, NavParams, ViewController } from 'ionic-angular';
 })
 export class SettingsModalPage {
     showDecimals: boolean;
+    toggleUnits: boolean;
 
     constructor(public params: NavParams, public viewCtrl: ViewController) {
       this.showDecimals = params.get('showDecimals');
+      this.toggleUnits = params.get('metric');
     }
 
     close(){
-        this.viewCtrl.dismiss(this.showDecimals);
+        this.viewCtrl.dismiss({showDecimals: this.showDecimals, metric: this.toggleUnits});
     }
  }
